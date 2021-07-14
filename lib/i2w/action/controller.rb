0249a607a5e540d2f55d0a21195f0b92d97cfd1b
Ian White <ian.w.white@gmail.com>
2021-07-14 12:46:55 +1000

    Use namespaced classref for action_class, and memoize it

--- lib/i2w/action/controller.rb
+++ lib/i2w/action/controller.rb
@@ -10,17 +10,23 @@ module I2w
 
       included do
         extend Repo::Class
+        extend Memoize
 
         def self.repo_class_base_name = controller_path.singularize.classify
 
         def self.repo_class_ref(type) = PossiblyNamespacedRepoClassRef.new(repo_class_base_name, type)
 
         repo_class_accessor :repository, :input, :model
+
+        memoize def action_class(action_name)
+          class_name = "#{name.sub(/Controller\z/, '')}::#{action_name.to_s.classify}Action"
+          PossiblyNamespacedRepoClassRef.new(class_name, :action).lookup
+        end
       end
 
       private
 
-      delegate :repository_class, :input_class, :model_class, to: 'self.class', private: true
+      delegate :repository_class, :input_class, :model_class, :action_class, to: 'self.class', private: true
 
       def action(action_name = self.action_name)
         action_class(action_name).new(**action_dependencies)
@@ -28,21 +34,6 @@ module I2w
 
       def action_dependencies
         { repository_class: repository_class, input_class: input_class }
-      end
-
-      # TODO: memoize on class with cache based on action_name
-      def action_class(action_name)
-        candidates = action_class_candidates(action_name)
-        candidates.each do |candidate|
-          return "#{self.class.module_parent}::#{candidate.classify}".constantize
-        rescue NameError
-          next
-        end
-        raise NameError, "Couldn't find action class for #{action_name} #{model_class} in #{candidates}"
-      end
-
-      def action_class_candidates(action_name)
-        ["#{controller_name}/#{action_name}_action", "#{action_name}_action"]
       end
 
       def action_attributes(input_class = self.input_class)
