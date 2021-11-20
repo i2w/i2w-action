5de2edc141c9f248533259051fa2f6a193f1db6c
Ian White <ian.w.white@gmail.com>
2021-11-20 13:36:12 +1100

    SetResult#call only takes kwargs, improve comments

--- lib/i2w/action/set_result.rb
+++ lib/i2w/action/set_result.rb
@@ -1,13 +1,13 @@
 module I2w
   class Action
-    # include to have #call create an open_result and pass it to #set_open_result, and return the result
+    # include to have #call create an open_result and pass it to #set_result, and return the result
     # You will perform the logic of the action in #set_result, and it works like 'do' notation, the first
     # failure will exit early
     #
     # If you want to process some arguments, and setup the result accordingly, perhaps in a superclass or mixin,
-    # then use #before_set_result, which is allowed to mutate the arguments array
+    # then use #before_set_result, which must return any unprocessed kwargs.
     module SetResult
-      def call(*args, **kwargs)
+      def call(**kwargs)
         open_result do |result|
           unprocessed_kwargs = before_set_result(result, **kwargs)
           set_result(result, **unprocessed_kwargs)
