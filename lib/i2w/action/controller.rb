8cf4875401108ce8c86aa0453748e2fab7eadbfd
Ian White <ian.w.white@gmail.com>
2021-11-28 14:52:09 +1100

    Docs

--- lib/i2w/action/controller.rb
+++ lib/i2w/action/controller.rb
@@ -4,7 +4,7 @@ require_relative 'controller/crud_actions'
 
 module I2w
   class Action
-    # mixin for ActionController::Base
+    # mixin for Rails controllers to help with using actions
     module Controller
       extend ActiveSupport::Concern
 
@@ -18,22 +18,36 @@ module I2w
 
       private
 
+      # instantiate (with #dependencies) an Action class based on conventional group naming
       def action(action_name) = action_class(action_name).new(**dependencies)
 
+      # call the action, and render the result, use this when you don;t want to handle failure
       def render_action(action_name, template_name = action_name, **kwargs)
         render template_name.to_s, locals: { **locals, **action(action_name).call(**kwargs) }
       end
 
+      # the arguments used to instantiate an Action class for this controller
       def dependencies = { repository_class: repository_class, input_class: input_class }
 
+      # given an input class, return the attributes as a hash from #params for the input
       def attributes(input_class = self.input_class)
-        params.require(input_class.model_name.param_key).permit(*input_class.attribute_names).to_h.symbolize_keys
+        params.require(input_class.model_name.param_key)
+              .permit(*input_class.attribute_names)
+              .to_h.symbolize_keys
+
       rescue ActionController::ParameterMissing
         {}
       end
 
-      def parameters(*keys, **defaults) = { **defaults, **params.permit(*keys, *defaults.keys).to_h.symbolize_keys }
+      # slice the given keys from #params, with optional defaults filled in (returns hash)
+      def parameters(*keys, **defaults)
+        parameters = params.permit(*keys, *defaults.keys)
+                           .to_h.symbolize_keys
 
+        { **defaults, **parameters }
+      end
+
+      # override this to add to the locals that are passed to render in #render_action
       def locals = {}
     end
   end
